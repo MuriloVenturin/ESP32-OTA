7af962d8ff364023e701c8ce50e5d4e365953c72
MuriloVenturin <murilo.venturin@gmail.com>
2019-05-03 17:35:57 -0300

    versão 0

---
+++ main/main.py
@@ -0,0 +1,19 @@
+from machine import Pin
+from time import sleep
+from ota_update.main.ota_updater import OTAUpdater
+
+def download_and_install_update_if_available():
+  o = OTAUpdater('url-to-your-github-project')
+  o.download_and_install_update_if_available('wifi-ssid', 'wifi-password')
+
+def start():
+  led = Pin(2, Pin.OUT)
+  while True:
+    led.value(not led.value())
+    sleep(0.5)
+
+def boot():
+  download_and_install_update_if_available()
+  start()
+  boot()
+
