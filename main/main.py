60880d46974a83ba32aa02f2499537df6b5d093a
pinazawa <pedro.inazawa@gmail.com>
2019-05-06 19:02:04 -0300

    Mudanca

--- main/main.py
+++ main/main.py
@@ -1,19 +1,24 @@
 from machine import Pin
 from time import sleep
-from ota_update.main.ota_updater import OTAUpdater
+from ota_updater import OTAUpdater
 
 def download_and_install_update_if_available():
-  o = OTAUpdater('url-to-your-github-project')
+  o = OTAUpdater('https://github.com/MuriloVenturin/ESP32-OTA')
   o.download_and_install_update_if_available('RunPal-GUEST', 'runpalbr')
 
 def start():
   led = Pin(2, Pin.OUT)
   while True:
-    led.value(0)
+    led.value(not led.value())
     sleep(0.5)
 
 def boot():
   download_and_install_update_if_available()
   start()
   boot()
+
+
+if __name__ == '__main__':
+	
+	boot()
 
